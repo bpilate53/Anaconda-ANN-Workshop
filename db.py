e0ecbba70c0c5521e9e6acefa96c402f3788e1ca
Barbara <Barbara@Barbaras-MBP.attlocal.net>
2022-10-04 08:38:51 -0500

    update

--- db.py
+++ db.py
@@ -5,9 +5,9 @@ import pymongo
 class Database:
 
     def get_database(self):
-        #db_client = pymongo.MongoClient('localhost', 27017)
+        db_client = pymongo.MongoClient('localhost', 27017)
        
-        db_client = pymongo.MongoClient("mongodb+srv://admin:8SLKvfeaChjpi9Zy@cluster0.tz99yuz.mongodb.net/?retryWrites=true&w=majority")
+        #db_client = pymongo.MongoClient("mongodb+srv://admin:8SLKvfeaChjpi9Zy@cluster0.tz99yuz.mongodb.net/?retryWrites=true&w=majority")
         
         prediction_db = db_client.prediction_db
 
